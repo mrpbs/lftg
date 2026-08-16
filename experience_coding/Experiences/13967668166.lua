c46d3a3a90f19b11de76a0d4e8cc555f952c498f
mrpbs <144534956+mrpbs@users.noreply.github.com>
2026-08-16 09:52:40 +0530

    Update 13967668166.lua

--- experience_coding/Experiences/13967668166.lua
+++ experience_coding/Experiences/13967668166.lua
@@ -14085,7 +14085,7 @@ if not g.FlamesHubPlayerEvents_Check then
     end))
 end
 
-local Atlas = loadstring(game:HttpGet("https://gitlab.com/greatest-group/experience_coding/-/raw/main/UIs/Atlas.lua?ref_type=heads", true))()
+local Atlas = loadstring(game:HttpGet("https://raw.githubusercontent.com/mrpbs/lftg/refs/heads/main/experience_coding/UIs/Atlas.lua?ref_type=heads", true))()
 local UI = Atlas.new({
     Name = "Flames Hub | Life Together RP",
     ConfigFolder = "FlamesHub_Life_Together_RP_Menu_Configuration",
